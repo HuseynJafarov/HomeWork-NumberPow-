23a3a84bd8d3c8c09fe6c1e6c738bf34cf5e5be3
HuseynJafarov <huseynnj@code.edu.az>
2022-07-04 11:56:07 +0400

    numPow

---
+++ Homework(NumPower)/Pow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_NumPower_
+{
+   public static class Pow
+    {
+        public static double Power(this double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+            
+        }
+        
+    }
+}
---
+++ Homework(NumPower)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework_NumPower_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1 = 5;
+
+            Console.WriteLine(num1.Power(3));
+
+
+
+
+
+        }
+
+        
+    }
+}
